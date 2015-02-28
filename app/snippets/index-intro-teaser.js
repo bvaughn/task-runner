dccb1f6d95d53927389114310737609cc1c27e0e
bvaughn <brian.david.vaughn@gmail.com>
2015-02-28 10:39:02 -0800

    Updated intro

--- app/snippets/index-intro-teaser.js
+++ app/snippets/index-intro-teaser.js
@@ -1,7 +1,19 @@
-new tr.Chain()              // Chain together tasks to...
-  .first(initializeSession) // Initialize the session.
-  .or(requireSignIn)        // Require sign-in if no session.
-  .then(loadUserProfile)    // Load the session-user's profile.
-  .or(setupUserProfile)     // If no profile exists create one.
-  .then(showHomeScreen)     // Lastly, show the home screen.
+var chain = new tr.Chain()
+  .first(initSession) // Initialize the session.
+  .or(requireAuth)    // Require sign-in if no session.
+  .then(loadProfile)  // Load the session-user's profile.
+  .or(setupProfile)   // If no profile exists create one.
+  .then(goToHome)     // Lastly, show the home screen.
   .run();
+
+// The above sequence can be interrupted easily,
+// No matter which task is running...
+chain.interrupt();
+
+// The sequence can later be resumed,
+// And will pick back up where it was interrupted...
+chain.run();
+
+// Listeners can be notified of state changes...
+chain.errored(failureCallback);
+chain.completed(successCallback)
