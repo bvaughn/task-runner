f823a75a09bea97ecfabdf09488db44f9f6f5feb
bvaughn <brian.david.vaughn@gmail.com>
2015-03-01 14:06:10 -0800

    Renamed DOM element to fix race

--- app/snippets/tutorials/type-demo.js
+++ app/snippets/tutorials/type-demo.js
@@ -1,7 +1,9 @@
+var domElement = document.getElementById('typingWithIntervalHeader');
+
 new tr.Chain()
-  .first(new Type(typedHeader, "This tutorial shows how to use the built-in Interval task to create a typing effect."))
+  .first(new Type(domElement, "This tutorial shows how to use the built-in Interval task to create a typing effect."))
   .then(new tr.Sleep(2000))
-  .then(new Untype(typedHeader))
+  .then(new Untype(domElement))
   .then(new tr.Sleep(1000))
   .completed(function(task) {
     task.reset();
