4b9d6c276ecab87ad327ba3dd5d6cc1dc57cea3e
bvaughn <brian.david.vaughn@gmail.com>
2015-03-03 07:34:32 -0800

    Made a few changes to index page based on feedback from Mark

--- app/snippets/index-intro-teaser-3.js
+++ app/snippets/index-intro-teaser-3.js
@@ -1,16 +1,12 @@
 new tr.Xhr('/path/to/load')
   .started(
-    function() {
-      // Called whenever the task is run.
-    })
-  .completed(
+    function(task) {}
+  ).completed(
     function(task) {
-      // Called once the XHR completes.
       task.getData(); // Contains response data.
-    })
-  .errored(
+    }
+  ).errored(
     function(task) {
-      // Called in the event of an error!
       task.getError(); // Error context.
       task.getErrorMessage(); // Additional error info.
     });
